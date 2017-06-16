b25546a0cf01df72470ab4f9e01993cc24ab687f
Jannis R <mail@jannisr.de>
2017-06-16 23:49:46 +0200

    simple tests :white_check_mark:

--- test.js
+++ test.js
@@ -2,7 +2,36 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const createResponder = require('.')
+const {decorate, SUSPEND, INSERT, coroutine} = createResponder
 
-test('todo', (t) => {
+test('coroutine stops at SUSPEND', (t) => {
+	function* run () {
+		yield Promise.resolve(1)
+		const p = Promise.resolve(2)
+		decorate(p, SUSPEND)
+		yield p
+		// should never get here
+		yield Promise.resolve(3)
+	}
+
+	coroutine(run(), Promise.resolve(), 'foo')
+	.then((val) => {
+		t.equal(val, 2)
+		t.end()
+	})
+	.catch(t.ifError)
+})
+
+test('coroutine passes in at INSERT', (t) => {
+	function* run () {
+		const p = Promise.resolve(2)
+		decorate(p, INSERT)
+		const x = yield p
+		t.equal(x, 'foo')
+		t.end()
+	}
+
+	coroutine(run(), Promise.resolve(), 'foo')
+	.catch(t.ifError)
 })
